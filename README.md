95e74ec77d5fe9cd730e4f28e1e84d4a5e504c05
anonekama <anonymous.nekama@gmail.com>
2023-05-14 10:55:23 -0400

    Working through fastapi tutorial

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 * "uvicorn[standard]"
 
 Developed on python 3.8.2
+
+# Planning
+
+* cyoa_parent
+* cyoa_version (default: Version A)
+* post (imgchest, reddit post, 4chan thread) -- a collection of images/links
+* link (direct link)
--- api/main.py
+++ api/main.py
@@ -1,4 +1,32 @@
-from fastapi import FastAPI, APIRouter
+from pathlib import Path
+from typing import Optional, Any
+from fastapi import FastAPI, APIRouter, Query, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+
+from api.schemas import Recipe, RecipeSearchResults, RecipeCreate
+
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
+    },
+]
 
 # 1
 app = FastAPI(
@@ -8,13 +36,68 @@ app = FastAPI(
 # 2
 api_router = APIRouter()
 
+@api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
+def fetch_recipe(*, recipe_id: int) -> Any:
+    """
+    Fetch a single recipe by ID
+    """
+
+    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if not result:
+        # the exception is raised, not returned - you will get a validation
+        # error otherwise.
+        # 2
+        raise HTTPException(
+            status_code=404, detail=f"Recipe with ID {recipe_id} not found"
+        )
+
+    return result[0]
+
+@api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
+def search_recipes(
+    *,
+    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),  # 2
+    max_results: Optional[int] = 10
+) -> dict:
+    """
+    Search for recipes based on label keyword
+    """
+    if not keyword:
+        # we use Python list slicing to limit results
+        # based on the max_results query parameter
+        return {"results": RECIPES[:max_results]}
+
+    results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
+    return {"results": list(results)[:max_results]}
+
+@api_router.post("/recipe/", status_code=201, response_model=Recipe)
+def create_recipe(*, recipe_in: RecipeCreate) -> dict:  # 2
+    """
+    Create a new recipe (in memory only)
+    """
+    new_entry_id = len(RECIPES) + 1
+    recipe_entry = Recipe(
+        id=new_entry_id,
+        label=recipe_in.label,
+        source=recipe_in.source,
+        url=recipe_in.url,
+    )
+    RECIPES.append(recipe_entry.dict())  # 3
+
+    return recipe_entry
+
 # 3
 @api_router.get("/", status_code=200)
-def root() -> dict:
+def root(request: Request) -> Any:  # 2
     """
-    Root Get
+    Root GET
     """
-    return {"msg": "Hello, World!"}
+
+    # 3
+    return TEMPLATES.TemplateResponse(
+        "index.html",
+        {"request": request, "recipes": RECIPES},
+    )
 
 # 4
 app.include_router(api_router)
