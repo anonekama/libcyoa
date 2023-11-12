10b212965905054b82b01a747bb24b674df19a88
anonekama <anonymous.nekama@gmail.com>
2023-11-12 16:11:06 -0500

    Upgrated packages

--- libcyoa/models/link.py
+++ libcyoa/models/link.py
@@ -10,5 +10,27 @@ class Link(Base):
     url = Column(String(256), index=True)
     title = Column(String(256), nullable=True)
     host = Column(String(256), nullable=True)
-    submitter_id = Column(String(10), ForeignKey("user.id"), nullable=True)
-    submitter = relationship("User", back_populates="links")
+    # Date published
+    # Version
+    # Part
+    # Status (WIP, complete)
+    # Media (image, interactive, text)
+
+    # Images = relationship
+    # Page_count
+    # Word_count
+    # Pixel_count
+    # Full_text
+    # Keybert_keywords
+
+    # isCurrent
+    # isBroken
+    # isParsed
+
+    submitter_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    submitter = relationship("User")
+
+class LinkHistory(Base):
+    """Reversion history of Link."""
+    id = Column(Integer, primary_key=True, index=True)
+    link_id = Column(Integer, ForeignKey("link.id"))
